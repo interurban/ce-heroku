74f571bd59259d5e508f989ebbb01b59a1602cb5
bruno bornsztein <bruno.bornsztein@gmail.com>
2011-10-06 10:59:34 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'community_engine', '2.0.0.beta', :git => 'https://github.com/bborn/communityengine.git', :branch => 'rails3'
+# gem 'community_engine', '2.0.0.beta', :path => '~/Projects/rails3.1/community_engine', :branch => 'rails3'
+
+gem 'sqlite3'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ce3Test::Application.config.secret_token = 'd250fd4dcfe2bd687bfcb499fc7221b5f35a383f816275fd045d28ab6858672de3050fa499ba917230f500041bbb7a3690978714bad54c6abe2524c1d9c1969a'
